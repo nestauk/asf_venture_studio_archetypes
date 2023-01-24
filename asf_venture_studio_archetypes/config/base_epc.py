d763191df5a440a0987a215dfcee57b8bc13103a
EnricoGavagnin <gavagnin.enrico@gmail.com>
2023-01-24 17:31:04 +0000

    added list of selected features

---
+++ asf_venture_studio_archetypes/config/base_epc.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+selected_feat = pd.read_csv(
+    "asf_venture_studio_archetypes/config/epc_feature_selection.csv"
+)
+
+EPC_FEAT_SELECTION = selected_feat.feature[selected_feat.keep].tolist()
+
+EPC_PREP_FEAT_SELECTION = selected_feat.feature[
+    selected_feat.keep & selected_feat.clean
+].tolist()
+
+EPC_PREP_CLEAN_FEAT_SELECTION = selected_feat.feature[
+    selected_feat.keep & selected_feat.clean & ~selected_feat.notes.notna()
+].tolist()
+
+EPC_PREP_CLEAN_USE_FEAT_SELECTION = selected_feat.feature[
+    selected_feat.keep
+    & selected_feat.clean
+    & ~selected_feat.notes.notna()
+    & selected_feat.use
+].tolist()
