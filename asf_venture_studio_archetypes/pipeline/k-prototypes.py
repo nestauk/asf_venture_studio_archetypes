d7286839d55044ab2c61ed8a16c004fda3a5da9d
imy99 <imanmuse@NESTA-FVFFP149Q05P.broadband>
2023-03-20 16:06:48 +0000

    adding umap_params_comparisons

--- asf_venture_studio_archetypes/pipeline/k-prototypes.py
+++ asf_venture_studio_archetypes/pipeline/k-prototypes.py
@@ -26,12 +26,7 @@ num_feat = base_epc.EPC_FEAT_NUM_KPROTO
 cat_feat = base_epc.EPC_FEAT_CAT_KPROTO
 all_feat = base_epc.EPC_FEAT_ALL_KPROTO
 n_sample = 5000
-
 prep_epc = load_data(all_feat, n_sample)
-# processed_data = process_data(prep_epc, num_feat, cat_feat, oh_encoder=True)
-# prep_epc = load_data(all_feat, n_sample)
-# original_categories = process_data(prep_epc, num_feat, cat_feat)
-# prep_epc = load_data(all_feat, n_sample)
 
 
 def KProto_apply(
@@ -42,13 +37,28 @@ def KProto_apply(
     n_sample=n_sample,
     n_clusters=4,
     n_neighbors=30,
-    min_dist=0.0,
+    min_dist=0.1,
 ):
+    """
+
+    Args:
+        df (pd.DataFrame): Data to perform K-prototypes on.
+        dimension(int): Preforming umap dimensionality reduction to chosen dimension
+        num_feat(list): List of strings refering to the numerical features
+        all_feat(list): List of strings referring to all features, categorical and numerical combined
+
+    Returns:
+        kproto:
+        pd.DataFrame: Data with clustering labels annotations
+    """
+
     start_time = time.time()
     print("Performing K-prototypes.")
 
     processed_data = process_data(prep_epc.copy(), num_feat, cat_feat, oh_encoder=True)
-    original_categories = process_data(prep_epc.copy(), num_feat, cat_feat)
+    original_categories = process_data(
+        prep_epc.copy(), num_feat, cat_feat, scaler=False
+    )
 
     numerical = processed_data[num_feat]
     categorical = processed_data.drop(columns=num_feat)
@@ -134,12 +144,10 @@ def KProto_apply(
     runtime = round((end_time - start_time) / 60)
     print("K-prototypes took {} minutes.\n".format(runtime))
 
-    return kproto
+    return kproto, original_categories
 
 
-def KProto_elbow_method(
-    kprot_data_date="2023-03-15", num_feat=num_feat, max_cluster=16
-):
+def KProto_elbow_method(kprot_data_date, num_feat=num_feat, max_cluster=16):
     kprot_data = pd.read_csv(
         f"outputs/data/kproto-{kprot_data_date}-ohe_catgories.csv"
     ).copy()
@@ -172,16 +180,14 @@ def KProto_elbow_method(
 def KProto_silhouette_analysis(
     n_cluster_vals, plot: bool = True, num_feat=num_feat
 ) -> int:
-    """Perform Silhouette analysis with K-means to determine optimal number of cluster
+    """Perform Silhouette analysis with K-prototypes to determine optimal number of cluster
 
     Args:
-        df (pd.DataFrame): Data to use for clustering (numerical)
-        n_cluster_vals (Iterator): Range of cluster numners to use of the S-analysis
-        plot (bool, optional): Plot and save plot. Defaults to True.
 
     Returns:
         int: Optimal number of clusters.
     """
+    # doesn't work currently
 
     start_time = time.time()
     print("Performing Silhouette analysis.")
@@ -192,13 +198,8 @@ def KProto_silhouette_analysis(
     X = df.values
     sil_coeff = np.zeros(np.shape(n_cluster_vals))
     for i, n_cluster in enumerate(n_cluster_vals):
-        kproto = KProto_apply(n_clusters=n_cluster).fit(X)
-        print(f"{kproto}")
-        catColumnsPos = [
-            df.columns.get_loc(col) for col in list(df.drop(columns=num_feat))
-        ]
-        print(f"{catColumnsPos}")
-        label = kproto.fit_predict(df, categorical=catColumnsPos)
+        kproto, trash = KProto_apply(n_clusters=n_cluster).fit(X)
+        label = kproto.labels_
         sil_coeff[i] = silhouette_score(X, label, metric="euclidean")
 
     if plot:
@@ -220,6 +221,44 @@ def KProto_silhouette_analysis(
     return list(n_cluster_vals)[list(sil_coeff).index(max(sil_coeff))]
 
 
+def umap_params_fig(
+    n_neighbors,
+    min_dist,
+    date="2023-03-17",
+    tooltip=[
+        "INSPECTION_DATE",
+        "PROPERTY_TYPE",
+        "BUILT_FORM",
+        "HEATING_SYSTEM",
+        "HEATING_FUEL",
+        "ROOF_TYPE",
+        "WALL_TYPE",
+        "WALLS_INSULATION",
+        "TRANSACTION_TYPE",
+    ],
+):
+    trash, original_categories = KProto_apply(
+        n_neighbors=n_neighbors, min_dist=min_dist
+    )
+
+    fig = (
+        alt.Chart(original_categories)
+        .mark_circle(size=10)
+        .encode(x="x", y="y", color="clusters:N", tooltip=tooltip)
+        .properties(title=f"{n_neighbors}-{min_dist}")
+    )
+
+    return fig
+
+
+def save_umap_params_comparisons(n_neighbors_range=range(30, 50, 5)):
+    figs = []
+    for n_neighbor in n_neighbors_range:
+        min_dist = 1
+        figs.append(umap_params_fig(n_neighbors=n_neighbor, min_dist=min_dist))
+        for fig in figs:
+            fig.save(f"outputs/figures/comparing_plots-{n_neighbor}-{min_dist}.html")
+
+
 if __name__ == "__main__":
-    KProto_apply()
-    # KProto_silhouette_analysis(n_cluster_vals = [2,3,4])
+    save_umap_params_comparisons()
